1500f04f9e111758f859714ef6e54033349fcf98
Shredhaya <shredhayagoel@gmail.com>
2026-09-07 15:29:13 +0530

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,86 +1,22 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import ForgotPassword from "./pages/forgotpassword";
+import NewPassword from "./pages/newpassword";
 
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [age, setAge] = useState("");
-  const [mobile, setMobile] = useState("");
-  const [address, setAddress] = useState("");
-  const [message, setMessage] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const response = await fetch("http://localhost:3000/users/signup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: name,
-        email: email,
-        age: age,
-        mobile: mobile,
-        address: address,
-      }),
-    });
-
-    const data = await response.json();
-
-    setMessage(data.message);
-    console.log(data);
-    alert("user created succesfully");
-  };
-
   return (
-    <div>
-      <h1>Signup</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-
-        <br />
-
-        <input
-          type="email"
-          placeholder="Enter email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <br />
-
-        <input
-          type="number"
-          placeholder="Enter age"
-          value={age}
-          onChange={(e) => setAge(e.target.value)}
-        />
-
-        <input
-          type="number"
-          placeholder="Enter no"
-          value={mobile}
-          onChange={(e) => setMobile(e.target.value)}
-        />
-
-        <input
-          type="text"
-          placeholder="Enter address"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-        />
-
-        <br />
-
-        <button type="submit">Signup</button>
-      </form>
-    </div>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/newpassword" element={<NewPassword />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
