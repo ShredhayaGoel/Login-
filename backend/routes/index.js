d74a1fa660bc7c057321a4afbd61c1d83b86cd76
Shredhaya <shredhayagoel@gmail.com>
2026-09-07 23:02:43 +0530

    render login at/login

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
   res.render('signup', { title: 'Express' });
 });
+router.get('/login', function (req, res, next) {
+  res.render('login', { title: 'Express' });
+});
 
 module.exports = router;
