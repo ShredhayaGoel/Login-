227eb53992ccc1cac856037489ea92b7180d281a
Shredhaya <shredhayagoel@gmail.com>
2026-09-13 23:55:47 +0530

    verify otp

---
+++ frontend/src/pages/verifyoyp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Verifyotp = () => {
+  return (
+    <div>
+      <div>
+        <label>Enter OTP</label>
+        <input type="text" placeholder="Enter OTP" />
+      </div>
+    </div>
+  );
+};
+
+export default Verifyotp;
